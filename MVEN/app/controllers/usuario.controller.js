1bad6807ccae3eff79356bde3a91987f65ded084
dangr400 <dangr401@gmail.com>
2021-11-02 11:04:28 +0100

    Añadidos métodos para añadir contactos en usuario

--- MVEN/app/controllers/usuario.controller.js
+++ MVEN/app/controllers/usuario.controller.js
@@ -2,41 +2,94 @@ const config = require("../config/auth.config");
 const db = require("../models");
 const User = db.usuario;
 const Grupos = db.grupos;
+const Peticion = db.peticion;
 
 exports.verContactos = (req, res) => {
-  User.findById(req.body.userID, 'contactos')
-    .populate('contactos')
+  User.findById(req.userId, 'contactos')
+    .populate("contactos")
     .exec((err, contact) => {
       if (err) {
         res.status(500).send({message: "error en el servidor"});
       }
       if (!contact){
-        return res.status(404).send({ message: "No existe el usuario." });
+        res.status(404).send({ message: "No existe el usuario." });
+        return;
       }
       let listaContactos = [];
       contact.contactos.forEach(element => listaContactos.push(element.username))
-      res.status(200).send(listaContactos);
+      res.status(200).send(contact);
     });
 }
 
 exports.enviarPeticionContacto = (req, res) => {
-  User.findByIdAndUpdate(req.userId, {$push: {peticiones_contacto_enviadas: req.body.contactoId}})
-  User.findByIdAndUpdate(
-    req.userId,
-    {$push:
-      {
-        peticiones_contacto_enviadas: req.body.contactoId
-      }
-    },
-    function (err, exito) {
-      if (err){
-        res.status(500).send({message: "no se ha podido enviar la peticion"})
-      }
-      else {
-        res.status(200).send({message: "peticion enviada"})
-      }
+  const nuevaPeticion = new Peticion({
+    idEmisor: req.userId,
+    idReceptor: req.body.contactoId,
+    estado: "PENDIENTE",
+    fecha: new Date(),
+  })
+  nuevaPeticion.save((err, peticion) => {
+    if (err) {
+      res.status(500).send({ message: err});
+      return;
     }
-  )
+    res.status(200).send({ message: "Peticion Enviada"});
+  })
+}
+
+exports.aceptarPeticion = (req, res) => {
+  Promise.all([
+    Peticion.findById(req.body.peticionId),
+    User.findById(req.userId),
+    User.findById(req.body.emisorId)
+  ]).then(([peticion, usuario1, usuario2]) => {
+    usuario1.contactos.push(usuario2._id);
+    usuario2.contactos.push(usuario1._id);
+    usuario1.save();
+    usuario2.save();
+    peticion.delete();
+    res.status(200).send({message: "Contacto añadido"});
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).send({message: "Hubo un error en el proceso"});
+    return;
+  });
+}
+
+exports.cancelarPeticion =(req, res) => {
+  Peticion.findByIdAndDelete(req.body.peticionId)
+  .exec((err, confirm) => {
+    if (err) {
+      res.status(500).send({ message: err});
+      return;
+    }
+    res.status(200).send({ message: "peticion cancelada"});
+  })
+
+}
+
+exports.verMisPeticiones = (req, res) => {
+  Peticion.find({idEmisor: req.userId})
+  .populate("idReceptor")
+  .exec((err, peticiones) =>{
+    if (err) {
+      res.status(500).send({ message: err});
+      return;
+    }
+    res.status(200).send(peticiones);
+  })
+}
+
+exports.verPeticionesPendientes = (req, res) => {
+  Peticion.find({idReceptor: req.userId})
+  .populate("idEmisor")
+  .exec((err, peticiones) =>{
+    if (err) {
+      res.status(500).send({ message: err});
+      return;
+    }
+    res.status(200).send(peticiones);
+  })
 }
 
 exports.misGrupos = (req, res) => {
@@ -49,6 +102,7 @@ exports.misGrupos = (req, res) => {
     .exec((err, grupos) => {
       if (err) {
         res.status(500).send({ message: "grupos no encontrados"});
+        return;
       }
       res.status(200).send(grupos);
     })
