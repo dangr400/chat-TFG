148bc07f3cdb7619df431ebb01bcdf7c7a148274
dangr400 <dangr401@gmail.com>
2021-11-02 11:05:02 +0100

    Creado modelo para la peticion de ser contacto

---
+++ MVEN/app/models/peticionContacto.model.js
@@ -0,0 +1,18 @@
+/**
+ * @file Módulo que describe el esquema de una peticion de contacto en la BD.
+ * @author Daniel Gomez Rodriguez
+ * @since 02.10.2021
+ * @returns mongoose.model
+ */
+ const mongoose = require("mongoose");
+
+ const Peticion = mongoose.model(
+   "PeticionContacto",
+   new mongoose.Schema({
+     idEmisor: {type: mongoose.Schema.Types.ObjectId, ref: "Usuario"},
+     idReceptor: {type: mongoose.Schema.Types.ObjectId, ref: "Usuario"},
+     estado: {type: String, default: "PENDIENTE"},
+     fecha: {type: Date, default: Date.now}
+   })
+ );
+ module.exports = Peticion;
