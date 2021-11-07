ce984f3dd22a8347e02973a1a97094e2d68ce82d
dangr400 <dangr401@gmail.com>
2021-11-08 00:33:32 +0100

    Separadas vistas de componentes

---
+++ VUE/src/views/Comms.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+        <h3>
+            <strong>Comunicaciones</strong>
+        </h3>
+        <button @click="activarContactos">Contactos</button>
+        <button @click="activarGrupos">Grupos</button>
+        <button @click="activarPeticiones">Peticiones</button>
+        <button @click="activarConversaciones">Acceder Conversacion</button>
+    </header>
+    <usuarioslist v-if="mostrarContactos"/>
+    <peticioneslist v-if="mostrarPeticiones"/>
+    <gruposlist v-if="mostrarGrupos" />
+    <conversacioneslist v-if="mostrarConversaciones" />
+  </div>
+</template>
+
+<script>
+import usuarioslist from '../components/Contactos.vue';
+import peticioneslist from '../components/Peticiones.vue';
+import gruposlist from '../components/Grupos.vue';
+import conversacioneslist from '../components/Conversaciones.vue';
+
+export default {
+    components: {
+        usuarioslist,
+        peticioneslist,
+        gruposlist,
+        conversacioneslist,
+    },
+    data() {
+        return {
+            mostrarContactos: true,
+            mostrarPeticiones: false,
+            mostrarGrupos: false,
+            mostrarConversaciones : false,
+        }
+    },
+    methods: {
+        activarContactos() {
+            this.mostrarContactos = true;
+            this.mostrarPeticiones = false;
+            this.mostrarGrupos = false;
+            this.mostrarConversaciones = false;
+        },
+        activarPeticiones() {
+            this.mostrarContactos = false;
+            this.mostrarPeticiones = true;
+            this.mostrarGrupos = false;
+            this.mostrarConversaciones = false;
+        },
+        activarGrupos() {
+            this.mostrarContactos = false;
+            this.mostrarPeticiones = false;
+            this.mostrarGrupos = true;
+            this.mostrarConversaciones = false;
+        },
+        activarConversaciones() {
+            this.mostrarContactos = false;
+            this.mostrarPeticiones = false;
+            this.mostrarGrupos = false;
+            this.mostrarConversaciones = true;
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/src/views/PageNotFound.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>
+            404: NO EXISTE LA DIRECCION
+        </h1>
+    </div>
+</template>
