0c2af993027164b74779921353454367560ce274
dangr400 <dangr401@gmail.com>
2021-11-09 17:33:53 +0100

    Arreglada vista de chat para funcionar con proyecto

---
+++ VUE/src/views/Chat.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+        <h3>
+            <strong>Chat</strong>
+        </h3>
+    </header>
+    <div>
+        <div class="left-panel">
+            <ul>
+                <li
+                    v-for="user in users"
+                    :key="user.userID"
+                />
+            </ul>
+        </div>
+        <message-panel
+        @input="onMessage"
+        :mensajes="mensajesR"
+        class="right-panel"
+        />
+    </div>
+    
+  </div>
+ 
+</template>
+
+
+<script>
+import ChatService from "../services/chat.service";
+import socket from "../socket.js";
+import MessagePanel from "../components/MessagePanel.vue";
+
+export default {
+    name: "chat",
+    data() {
+        return {
+            selectedUser: null,
+            users: new Array,
+            salaId: this.$route.params.salaId,
+            mensajes: new Array,
+        };
+    },
+    computed: {
+        mensajesR() {
+            return this.mensajes;
+        }
+    },
+    components: {
+        MessagePanel
+    },
+    methods: {
+        onMessage(content) {
+            if (typeof content == "string") {
+                socket.emit("enviarMensaje", {
+                mensaje: content,
+                sala: this.salaId,
+                });
+            }
+        },
+    },
+    mounted() {
+        socket.connect();
+        socket.emit('join', {sala: this.salaId, usuarioId: this.$store.state.auth.user});
+        socket.emit('identity', this.$store.state.auth.user);
+        ChatService.getConversacion(this.salaId)
+        .then(response => {
+            this.users = response.data.users;
+            this.mensajes.push(response.data.conversation);
+        });
+        socket.on('emitirMensaje', (response) => {
+            this.mensajes.push({
+                mensaje: response.mensaje,
+                emisor: response.emisor});
+        });
+        
+    },
+    unmounted() {
+        socket.emit('salirChat');
+        socket.off("connect");
+        socket.off("disconnect");
+    },
+}
+</script>
+
+<style>
+    .left-panel {
+        float: left;
+        width: 10%;
+        height: 100%;
+        overflow-x: hidden;
+        background-color: #3a3d3b;
+        color: white;
+    }
+    .right-panel {
+        margin-left: 260px;
+    }
+</style>
